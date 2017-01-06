32da493b801f97a9ceef2152c992c5e59dd5143d
soul11201 <soul11201@gmail.com>
2017-01-06 10:47:25 +0800

    hello world macOs application

---
+++ swiftlearn/main.swift
@@ -0,0 +1,15 @@
+//
+//  main.swift
+//  swiftlearn
+//
+//  Created by mlibai on 2017/1/6.
+//  Copyright © 2017年 yang. All rights reserved.
+//
+
+//import Foundation
+
+print("Hello, World!")
+
+
+
+
