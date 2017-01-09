2a88024258dfc003e0f21c46e3886e96bdb24cef
mlibai <imac@iMacdeiMac.local>
2017-01-09 17:28:29 +0800

    rename

---
+++ swiftlearn/swiftlearn/main.swift
@@ -0,0 +1,96 @@
+//
+//  main.swift
+//  swiftlearn
+//
+//  Created by mlibai on 2017/1/6.
+//  Copyright © 2017年 yang. All rights reserved.
+//
+
+//import Foundation
+
+print("Hello, World!")
+
+
+
+
+
+
+if let roomCount = john.residence?.numberOfRooms {
+    print("John's residence has \(roomCount) roots ")
+}else{
+    print("xxxxx")
+}
+
+
+
+
+var optional:String?="helo"
+
+if let someOptionalConstant = optional
+{
+    if someOptionalConstant.hasPrefix("hel")
+    {
+        print("begin with hel")
+    }
+    else
+    {
+        print("Not begin with hel")
+    }
+    
+}
+else
+{
+    print("val is null")
+}
+
+
+
+//
+// MARK: closure
+//
+
+let numbers = [4,3,2,8,7,6,9,10]
+
+let new = numbers.map{number in 2*number}
+
+let new1 = numbers.map({number in 2 * number})
+
+let new2 = numbers.map({
+    (number:Int) ->Int in
+    
+    return number * 2
+})
+
+
+
+print(new,new1,new2)
+
+let new4 = numbers.map{ $0 * 2} // equal to new
+print("new4",new4)
+
+
+
+
+// MARK: struct
+struct NamesTable
+{
+    let names:[String]
+    
+    subscript(index:Int) -> String
+    {
+        print("index \(index)")
+        return names[index]
+    }
+    
+}
+
+
+let namesTable = NamesTable(names: ["me","You","them"])
+
+let name = NamesTable[1]
+
+print("Nmae is \(name)")
+
+
+
+
---
+++ swiftlearn/swiftlearn/optional_chaining.swift
@@ -0,0 +1,26 @@
+//
+//  optional_chaining.swift
+//  swiftlearn
+//
+//  Created by mlibai on 2017/1/6.
+//  Copyright © 2017年 yang. All rights reserved.
+//
+
+//import Foundation
+
+class Person
+{
+    var residence:Residence?
+}
+
+class Residence
+{
+    var numberOfRooms = 1;
+}
+
+
+let john = Person()
+
+
+
+
